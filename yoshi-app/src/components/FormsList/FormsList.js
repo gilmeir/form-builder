0c0b3faa1b268715a3729c4a228e3d9b473bc7cd
Gil Meir <gilme@wix.com>
2019-02-16 17:24:56 +0200

    layout FormsList inside a Card

--- yoshi-app/src/components/FormsList/FormsList.js
+++ yoshi-app/src/components/FormsList/FormsList.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import Table from 'wix-style-react/Table';
 import Loader from 'wix-style-react/Loader';
 import Box from 'wix-style-react/Box';
+import Button from 'wix-style-react/Button';
+import Card from 'wix-style-react/Card';
+import Add from 'wix-style-react/new-icons/Add';
 import { formSubmitUrl, formSubmissionsUrl } from '../../routes/routes';
 
 class FormsList extends React.Component {
@@ -35,30 +38,46 @@ class FormsList extends React.Component {
     } = this.state;
 
     return (
-      <Box
-        align="center"
-        margin="50px"
-        direction="vertical"
-      >
-        <Box>
-          <Table
-            data={forms}
-            itemsPerPage={20}
-            columns={columns}
-            showHeaderWhenEmpty
-          />
-        </Box>
+      <Card>
+        <Card.Header
+          title="My Forms"
+          suffix={
+            <Link to="/builder" style={{textDecoration: 'none'}}>
+              <Button
+                size="medium"
+                prefixIcon={<Add />}
+              >
+                New Form
+              </Button>
+            </Link>
+          }
+        />
+        <Card.Content>
+          <Box
+            align="center"
+            direction="vertical"
+          >
+            <Box>
+              <Table
+                data={forms}
+                itemsPerPage={20}
+                columns={columns}
+                showHeaderWhenEmpty
+              />
+            </Box>
 
-        {
-          (loading || error) &&
-          <Box marginTop="25px">
-            <Loader
-              status={error ? 'error' : 'loading'}
-              text={error ? 'Failed loading your forms' : 'Loading your forms'}
-            />
+            {
+              (loading || error) &&
+              <Box marginTop="25px">
+                <Loader
+                  status={error ? 'error' : 'loading'}
+                  text={error ? 'Failed loading your forms' : 'Loading your forms'}
+                />
+              </Box>
+            }
           </Box>
-        }
-      </Box>
+        </Card.Content>
+      </Card>
     )
   }
 }
